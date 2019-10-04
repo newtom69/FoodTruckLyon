daee9e1ba6f14008ffcbb968e69faffe03c2d9d0
newtom69 <thomas@vuille.fr>
2019-10-04 21:30:39 +0200

    GetHash

--- testLinq/DAL/UtilisateurDAL.cs
+++ testLinq/DAL/UtilisateurDAL.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,23 @@ namespace testLinq.DAL
 {
     class UtilisateurDAL
     {
+
+
+
+
+
+
+
+        private static string GetHash(HashAlgorithm hashAlgorithm, string input)
+        {
+            byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
     }
 }
