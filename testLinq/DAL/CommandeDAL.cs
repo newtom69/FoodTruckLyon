5b74614b87761d7bfba186965a3b7bbff7e84d31
newtom69 <thomas@vuille.fr>
2019-10-04 21:36:04 +0200

    Connexion

--- testLinq/DAL/CommandeDAL.cs
+++ testLinq/DAL/CommandeDAL.cs
@@ -11,33 +11,35 @@ namespace testLinq.DAL
     {
         public void Ajouter(Commande laCommande)
         {
-            foodtruckEntities db = new foodtruckEntities();
-            db.Commande.Add(laCommande);
-            db.SaveChanges();
-            int idCommande = (from cmd in db.Commande
-                              where cmd.UtilisateurId == laCommande.UtilisateurId
-                              orderby cmd.Id descending
-                              select cmd.Id).FirstOrDefault();
-
-            List<Article> listeArticles = new List<Article>(); //TODO
-            ArticleDAL articleDAL = new ArticleDAL();
-            listeArticles.Add(articleDAL.Details(5));
-            listeArticles.Add(articleDAL.Details(1));
-            listeArticles.Add(articleDAL.Details(3)); 
-
-            foreach (var article in listeArticles)
+            using (foodtruckEntities db = new foodtruckEntities())
             {
-                int quantite = 1; //= article.Quantite; //TODO
-                double prixTotal = (article.Prix * quantite);
+                db.Commande.Add(laCommande);
+                db.SaveChanges();
+                int idCommande = (from cmd in db.Commande
+                                  where cmd.UtilisateurId == laCommande.UtilisateurId
+                                  orderby cmd.Id descending
+                                  select cmd.Id).FirstOrDefault();
 
-                Commande_Article cmdArt = new Commande_Article();
-                cmdArt.CommandeId = idCommande;
-                cmdArt.ArticleId = article.Id;
-                cmdArt.Quantite = quantite;
-                cmdArt.PrixTotal = prixTotal;
-                db.Commande_Article.Add(cmdArt);
+                List<Article> listeArticles = new List<Article>(); //TODO
+                ArticleDAL articleDAL = new ArticleDAL();
+                listeArticles.Add(articleDAL.Details(5));
+                listeArticles.Add(articleDAL.Details(1));
+                listeArticles.Add(articleDAL.Details(3));
+
+                foreach (var article in listeArticles)
+                {
+                    int quantite = 1; //= article.Quantite; //TODO
+                    double prixTotal = (article.Prix * quantite);
+
+                    Commande_Article cmdArt = new Commande_Article();
+                    cmdArt.CommandeId = idCommande;
+                    cmdArt.ArticleId = article.Id;
+                    cmdArt.Quantite = quantite;
+                    cmdArt.PrixTotal = prixTotal;
+                    db.Commande_Article.Add(cmdArt);
+                }
+                db.SaveChanges();
             }
-            db.SaveChanges();
         }
     }
 }
--- testLinq/DAL/UtilisateurDAL.cs
+++ testLinq/DAL/UtilisateurDAL.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using testLinq.Models;
 
 namespace testLinq.DAL
 {
     class UtilisateurDAL
     {
+        public Utilisateur Connexion(string email, string mdp)
+        {
+            string mdpHash;
+            using (SHA256 Hash = SHA256.Create())
+                mdpHash = GetHash(Hash, mdp);
 
+            Utilisateur lUtilisateur = new Utilisateur();
+            using (foodtruckEntities db = new foodtruckEntities())
+            {
+                lUtilisateur = (from user in db.Utilisateur
+                                where user.Email == email && user.Mdp == mdpHash
+                                select user).FirstOrDefault();
+            }
+            return lUtilisateur;
+        }
 
 
 
