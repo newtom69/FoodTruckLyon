63d9ae77f3cb1a2bfe5ee7a541e138a869f6511f
newtom69 <thomas@vuille.fr>
2019-10-05 23:24:02 +0200

    ajout roles admin utilisateur dans base de données

--- FoodTruck/Models/Utilisateur.cs
+++ FoodTruck/Models/Utilisateur.cs
@@ -28,6 +28,10 @@ namespace FoodTruck.Models
         public string Nom { get; set; }
         public string Prenom { get; set; }
         public string Telephone { get; set; }
+        public bool AdminCommande { get; set; }
+        public bool AdminUtilisateur { get; set; }
+        public bool AdminArticle { get; set; }
+        public bool AdminTotal { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Commande> Commande { get; set; }
