962fdf72536c3c822e1766f9ef2366f060fe9eb7
newtom69 <thomas@vuille.fr>
2019-10-05 08:38:54 +0200

    suppression using inutiles

--- FoodTruck/Controllers/CommandeController.cs
+++ FoodTruck/Controllers/CommandeController.cs
@@ -1,8 +1,5 @@
 ﻿using FoodTruck.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using FoodTruck.DAL;
 using System.Net.Mail;
--- FoodTruck/Controllers/UtilisateurController.cs
+++ FoodTruck/Controllers/UtilisateurController.cs
@@ -1,9 +1,5 @@
 ﻿using FoodTruck.DAL;
 using FoodTruck.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace FoodTruck.Controllers
