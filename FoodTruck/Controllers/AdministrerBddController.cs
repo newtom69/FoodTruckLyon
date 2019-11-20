6a9a38006263864bf550a3b0c60d686d40cec9f8
newtom69 <thomas@vuille.fr>
2019-11-20 05:08:58 +0100

    meilleure lisibilité

--- FoodTruck/Controllers/AdministrerBddController.cs
+++ FoodTruck/Controllers/AdministrerBddController.cs
@@ -16,25 +16,28 @@ namespace FoodTruck.Controllers
                 ViewBag.PanierProspectSupprimes = "Nombre d'enregistrements de PanierProspect supprimés : " + new PanierProspectDAL("").Purger(30);
                 ViewBag.OubliMotDePasseSupprimes = "Nombre d'enregistrements de OubliMotDePasse supprimés : " + new OubliMotDePasseDAL().Purger();
                 ViewBag.JourExceptionnelSupprimes = "Nombre d'enregistrements de JourExceptionnel supprimés : " + new JourExceptionnelDAL().Purger();
-
-                // purger dosssier image article
-                string dossierImage = Server.MapPath(ConfigurationManager.AppSettings["PathImagesArticles"]);
-                var fichiersPresents = (from fullFilename in Directory.EnumerateFiles(dossierImage)
-                                       select Path.GetFileName(fullFilename)).ToList();
-                
-                List<Article> tousArticles = new ArticleDAL().Tous();
-
-                var nomsImages = (from art in tousArticles
-                                  select art.Image.Trim()).ToList();
-
-                fichiersPresents.RemoveAll(nom => nomsImages.Contains(nom));
-                foreach (string fichier in fichiersPresents)
-                {
-                    System.IO.File.Delete(Path.Combine(dossierImage, fichier));
-                }
-                ViewBag.ImagesSupprimees = $"Nombre d'images supprimées : {fichiersPresents.Count}";
+                ViewBag.ImagesSupprimees = $"Nombre d'images supprimées : {PurgerImages()}";
             }
             return View();
+        }
+
+        private int PurgerImages()
+        {
+            string dossierImage = Server.MapPath(ConfigurationManager.AppSettings["PathImagesArticles"]);
+            var nomsFichiers = (from fullFilename in Directory.EnumerateFiles(dossierImage)
+                                select Path.GetFileName(fullFilename)).ToList();
+
+            List<Article> tousArticles = new ArticleDAL().Tous();
+
+            var nomsImages = (from art in tousArticles
+                              select art.Image.Trim()).ToList();
+
+            nomsFichiers.RemoveAll(nom => nomsImages.Contains(nom));
+            foreach (string fichier in nomsFichiers)
+            {
+                System.IO.File.Delete(Path.Combine(dossierImage, fichier));
+            }
+            return nomsFichiers.Count;
         }
     }
 }
